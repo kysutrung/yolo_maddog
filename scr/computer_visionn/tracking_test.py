2de6f153567f1b2a16f4aba1331dc2baca811771
trung_pc_l <trungnd2767@gmail.com>
2025-10-10 00:14:36 +0700

    test with drone footages

--- scr/computer_visionn/tracking_test.py
+++ scr/computer_visionn/tracking_test.py
@@ -3,12 +3,12 @@ from ultralytics import YOLO
 model = YOLO("yolo_weights/yolov8m.pt")
 
 model.track(
-    source="Motorcycle.mp4",                  
+    source="drone_footages/Whiteman01.mp4",                  
     conf=0.3,                  
     device="cuda",            
     classes=[0],               
     show=True,                 
     persist=True,              #bật tắt ID
     tracker="bytetrack.yaml",  #chọn tracker -> "botsort.yaml"
-    save=False                 #lưu video đầu ra
+    save=True                 #lưu video đầu ra
 )

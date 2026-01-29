dcf5592a46dfd73250da11b677ca69f5fc2ebf89
trung_pc_l <trungnd2767@gmail.com>
2026-01-29 15:46:50 +0700

    code check

--- scr/test_cases/main_test copy.py
+++ scr/test_cases/main_test copy.py
@@ -50,7 +50,7 @@ CONF_THRES     = 0.30
 CLASSES        = [0, 2, 3]
 PERSIST_ID     = True
 SAVE_OUTPUT    = False
-CAM_INDEX      = 0
+CAM_INDEX      = 1
 TARGET_FPS     = 30
 
 AX_LX, AX_LY, AX_RY, AX_RX = 0, 1, 2, 3

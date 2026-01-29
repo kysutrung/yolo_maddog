f7cfdce04c1ea04200a74fd8e6908510a4358b31
trung_pc_l <trungnd2767@gmail.com>
2026-01-29 15:01:09 +0700

    initialize new tests

---
+++ scr/big_test_01.py
@@ -0,0 +1,4 @@
+#ORB - Oriented FAST and Rotated BRIEF
+#Brute Force Matcher - tìm điểm chung giữa hai frame liên tiếp
+#RANSAC - Hạn chế các điểm match sai
+#Affine Transform
---
+++ scr/clahe_test.py
@@ -0,0 +1,1 @@
+#tăng tương phản hình ảnh
